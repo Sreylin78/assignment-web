45d808f28d4180747a15c2aa1fdc5f27351fc6e7
MORK-RAKSA <mork.raksa.1221@rupp.edu.kh>
2024-11-29 19:58:48 +0700

    raksa

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,31 +24,31 @@ if (links) {
   );
 }
 
-// Scroll Header Fixed
-const header = document.getElementById("header");
+// // Scroll Header Fixed
+// const header = document.getElementById("header");
 
-if (header) {
-  window.addEventListener("scroll", () => {
-    if (window.scrollY >= 100) {
-      header.classList.add("fixed-header");
-    } else {
-      header.classList.remove("fixed-header");
-    }
-  });
-}
+// if (header) {
+//   window.addEventListener("scroll", () => {
+//     if (window.scrollY >= 100) {
+//       header.classList.add("fixed-header");
+//     } else {
+//       header.classList.remove("fixed-header");
+//     }
+//   });
+// }
 
-// Scrollup
-const scrollup = document.getElementById("scrollup");
+// // Scrollup
+// const scrollup = document.getElementById("scrollup");
 
-if (scrollup) {
-  window.addEventListener("scroll", () => {
-    if (window.scrollY >= 180) {
-      scrollup.classList.add("show-scrollup");
-    } else {
-      scrollup.classList.remove("show-scrollup");
-    }
-  });
-}
+// if (scrollup) {
+//   window.addEventListener("scroll", () => {
+//     if (window.scrollY >= 180) {
+//       scrollup.classList.add("show-scrollup");
+//     } else {
+//       scrollup.classList.remove("show-scrollup");
+//     }
+//   });
+// }
 
 // Footer Year
 const year = document.getElementById("year");
@@ -56,3 +56,35 @@ const year = document.getElementById("year");
 if (year) {
   year.innerHTML = new Date().getFullYear();
 }
+
+
+// slider
+const slider = document.querySelector('.food-slider');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; // Adjust for faster scrolling
+  slider.scrollLeft = scrollLeft - walk;
+});
