e808ae4ea68d57a7da4be43735ca25c6cb2a5613
MENG Vuthvitou <mengvuthvitou11@gmail.com>
2024-12-03 15:42:00 +0700

    Add Structure for Menu Page

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,163 +1,30 @@
-// Render allmenu Cards
-const allMenuItems = [
-  {
-    id: 1,
-    image: "../assets/image/menu/menu-1.jpg",
-    title: "សាច់គោអាំងចង្កាក់",
-    description: "Description for សាច់គោអាំងចង្កាក់",
-    price: "៦៣.០០០៛",
-    stars: 4,
+const allMenu = new Swiper(".swiper-container", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
-  {
-    id: 2,
-    image: "../assets/image/menu/menu-2.jpg",
-    title: "ខ្យងផ្លិតឆាម្រេចខ្ទី",
-    description: "Description for ខ្យងផ្លិតឆាម្រេចខ្ទី",
-    price: "៥០.០០០៛",
-    stars: 4,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: true,
   },
-  {
-    id: 3,
-    image: "../assets/image/menu/menu-3.jpg",
-    title: "សាច់ក្កកតាកែវ",
-    description: "Description for សាច់ក្កកតាកែវ",
-    price: "៤៦.០០០៛",
-    stars: 3.5,
+  breakpoints: {
+    768: {
+      spaceBetween: 30,
+    },
+    1200: {
+      spaceBetween: 40,
+    },
   },
-  {
-    id: 4,
-    image: "../assets/image/menu/menu-4.jpg",
-    title: "ម៉ុកមី",
-    description: "Description for ម៉ុកមី",
-    price: "៤៦.០០០៛",
-    stars: 4,
-  },
-  {
-    id: 5,
-    image: "../assets/image/menu/menu-5.jpg",
-    title: "ភ្លាគ្រឿងសមុទ្រជាមួយផ្លែប៊ឺរ",
-    description: "Description for ភ្លាគ្រឿងសមុទ្រជាមួយផ្លែប៊ឺរ",
-    price: "៦៣.០០០៛",
-    stars: 4,
-  },
-  {
-    id: 6,
-    image: "../assets/image/menu/menu-6.jpg",
-    title: "ភ្លាសាច់គោ",
-    description: "Description for ភ្លាសាច់គោ",
-    price: "៤៦.០០០៛",
-    stars: 3,
-  },
-  {
-    id: 7,
-    image: "../assets/image/menu/menu-7.jpg",
-    title: "ញាំសាច់មាន់ជាមួយត្រយ៉ូងចេក",
-    description: "Description for ញាំសាច់មាន់ជាមួយត្រយ៉ូងចេក",
-    price: "២៩.០០០៛",
-    stars: 4,
-  },
-  {
-    id: 8,
-    image: "../assets/image/menu/menu-8.jpg",
-    title: "ស្ងោរជ្រក់បង្កង",
-    description: "Description for ស្ងោរជ្រក់បង្កង",
-    price: "១៦០.០០០៛",
-    stars: 4,
-  },
-  {
-    id: 9,
-    image: "../assets/image/menu/menu-9.jpg",
-    title: "សម្លម្ជូរខ្ទិះគ្រឿងសមុទ្រ",
-    description: "Description for សម្លម្ជូរខ្ទិះគ្រឿងសមុទ្រ",
-    price: "៧១.០០០៛",
-    stars: 3.5,
-  },
-  {
-    id: 10,
-    image: "../assets/image/menu/menu-10.jpg",
-    title: "សម្លកកូរ",
-    description: "Description for សម្លកកូរ",
-    price: "៤៦.០០០៛",
-    stars: 4,
-  },
-  {
-    id: 11,
-    image: "../assets/image/menu/menu-11.jpg",
-    title: "បាយឆាម្មាស់",
-    description: "Description for បាយឆាម្មាស់",
-    price: "៤២.០០០៛",
-    stars: 4,
-  },
-  {
-    id: 12,
-    image: "../assets/image/menu/menu-12.jpg",
-    title: "បាយការីសាច់មាន់ខ្ចប់ស្លឹកឈូក",
-    description: "Description for បាយការីសាច់មាន់ខ្ចប់ស្លឹកឈូក",
-    price: "៥៤.០០០៛",
-    stars: 3,
-  },
-  {
-    id: 13,
-    image: "../assets/image/menu/menu-13.jpg",
-    title: "បាយឆាបង្កង",
-    description: "Description for បាយឆាបង្កង",
-    price: "១៦០.០០០៛",
-    stars: 4,
-  },
-  {
-    id: 14,
-    image: "../assets/image/menu/menu-14.jpg",
-    title: "បាយឆាគ្រឿងសមុទ្រ",
-    description: "Description for បាយឆាគ្រឿងសមុទ្រ",
-    price: "៦៣.០០០៛",
-    stars: 4,
-  },
-  {
-    id: 15,
-    image: "../assets/image/menu/menu-15.jpg",
-    title: "ឆាខ្ញីសាច់មាន់",
-    description: "Description for ឆាខ្ញីសាច់មាន់",
-    price: "៨៤.០០០៛",
-    stars: 4,
-  },
-  {
-    id: 16,
-    image: "../assets/image/menu/menu-16.jpg",
-    title: "ប្រហុកខ្ទិះ",
-    description: "Description for ប្រហុកខ្ទិះ",
-    price: "៥៤,០០០៛",
-    stars: 3.5,
-  },
-  {
-    id: 17,
-    image: "../assets/image/menu/menu-17.jpg",
-    title: "សាច់គោអាំងទឹកម្រិច",
-    description: "Description for សាច់គោអាំងទឹកម្រិច",
-    price: "៧៥.០០០៛",
-    stars: 4,
-  },
-  {
-    id: 18,
-    image: "../assets/image/menu/menu-18.jpg",
-    title: "ទាអាំង",
-    description: "Description for ទាអាំង",
-    price: "៨៤.០០០៛",
-    stars: 4,
-  },
-  {
-    id: 19,
-    image: "../assets/image/menu/menu-19.jpg",
-    title: "ត្រីដុតអំបិល",
-    description: "Description for ត្រីដុតអំបិល",
-    price: "១៦០.០០០៛",
-    stars: 3,
-  },
-  {
-    id: 20,
-    image: "../assets/image/menu/menu-20.jpg",
-    title: "មាន់អាំងទឹកម្រិច",
-    description: "Description for មាន់អាំងទឹកម្រិច",
-    price: "៧៥.០០០៛",
-    stars: 4,
-  },
-];
+});
+
+const swiperContainer = document.querySelector(".swiper-container");
+swiperContainer.addEventListener("mouseenter", () => {
+  allMenu.autoplay.stop();
+});
+swiperContainer.addEventListener("mouseleave", () => {
+  allMenu.autoplay.start();
+});
